41e1201eecd0f7d1e5ce4a2d811dc2a2f1cc3ec5
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-01-05 18:33:35 -0800

    add second test case

--- src/testCases/firstTestCase.java
+++ src/testCases/firstTestCase.java
@@ -6,10 +6,20 @@ public class firstTestCase {
 
 	// TODO Auto-generated method stub
 	@Test
+	// TestNG doesn't require Java complier to run test cases
 	// Any method below @Test will be treated as a test case 
 	//with TestNG
 	public void Demo() {
 		System.out.println("Hello World");
+	}
+	
+	// By adding a second @Test, this is treated as a second
+	// test case. Every @Test with new method is treated as a
+	// new test case
+	
+	@Test
+	public void SecondTest() {
+		System.out.println("Good Bye");
 	}
 
 }
