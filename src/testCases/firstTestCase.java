6e6ddc264fc57879044382b91fdea520e1789461
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-01-05 18:28:43 -0800

    Add @Test annotation to invoke TestNG, remove Java compiler initial public static void main method and replace with Demo method to create first test case

---
+++ src/testCases/firstTestCase.java
@@ -0,0 +1,15 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class firstTestCase {
+
+	// TODO Auto-generated method stub
+	@Test
+	// Any method below @Test will be treated as a test case 
+	//with TestNG
+	public void Demo() {
+		System.out.println("Hello World");
+	}
+
+}
