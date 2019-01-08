6f8576de058eecf72bc16c9be7fa494194e0f3b3
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-01-08 10:16:07 -0800

    add forthDummyTest case with three test methods, use include tag in xml file to only include LoginAPIHomeLoan method test to run within ForthDummyTest case, use exclude tag in xml file to exclude MobileLoginCarLoan method test from executing within ThirdDummyTest cases

---
+++ src/testCases/FourthDummyTest.java
@@ -0,0 +1,24 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class FourthDummyTest {
+
+	@Test
+	public void WebLoginHomeLone() {
+		// Selenium code
+		System.out.println("web login home");
+	}
+	
+	@Test
+	public void MobileLoginHomeLoan() {
+		// Appium code
+		System.out.println("mobile login home");
+	}
+	
+	@Test
+	public void LoginAPIHomeLoan() {
+		// Rest API automation
+		System.out.println("API login home");
+	}
+}
---
+++ src/testCases/ThirdDummyTest.java
@@ -0,0 +1,24 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class ThirdDummyTest {
+
+	@Test
+	public void WebLoginCarLone() {
+		// Selenium code
+		System.out.println("web login car");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		// Appium code
+		System.out.println("mobile login car");
+	}
+	
+	@Test
+	public void LoginAPICarLoan() {
+		// Rest API automation
+		System.out.println("API login car");
+	}
+}
