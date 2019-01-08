211157e1593dd971b4c90b9f5df376af77ebccdc
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-01-08 10:35:15 -0800

    converted exlude mobile tag to a regex expression Mobile.* to target any test method that begins with the word Mobile, added three more test method of Mobile type to ThirdDummyTest case, created a second testng xml file which runs the whole testCases package using the packages and package tags

--- src/testCases/FourthDummyTest.java
+++ src/testCases/FourthDummyTest.java
@@ -17,6 +17,23 @@ public class FourthDummyTest {
 	}
 	
 	@Test
+	public void MobileLogoutHomeLoan() {
+		// Appium code
+		System.out.println("mobile logout home");
+	}
+	
+	@Test
+	public void MobileLoginHomeLoan2() {
+		// Appium code
+		System.out.println("mobile login home 2");
+	}
+	@Test
+	public void MobileLogoutHomeLoan2() {
+		// Appium code
+		System.out.println("mobile logout home 2");
+	}
+	
+	@Test
 	public void LoginAPIHomeLoan() {
 		// Rest API automation
 		System.out.println("API login home");
